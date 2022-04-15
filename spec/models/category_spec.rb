21ff873f1b0242cf1ddf9101da3673466775233b
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-15 11:42:43 +0700

    Add validation for Category, invalid without name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.factory' do
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:category)).to be_valid
+    end
+  end
+
+  describe '.validations' do
+    context 'with valid attributes' do
+      it 'is valid with a name' do
+        expect(FactoryBot.build(:category)).to be_valid
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'invalid without name' do
+        category = FactoryBot.build(:invalid_category)
+        category.valid?
+        expect(category.errors[:name]).to include("can't be blank")
+      end
+    end
+  end
 end

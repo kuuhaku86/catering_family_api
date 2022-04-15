179de8c4465f39a9dc90bfb133ab3a9bde692342
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-15 16:09:42 +0700

    Add factory for Customer model

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :customer do
-    name { "MyString" }
-    email { "MyString" }
+    name { "Name" }
+    email { "abc@gmail.com" }
+  end
+
+  factory :invalid_customer, parent: :customer do
+    name { nil }
+    email { nil }
   end
 end

692d8bad17b84556989bb777d7ff612b7048c611
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-16 11:29:22 +0700

    Add validation for Order model, invalid without total_price

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Order, type: :model do
         expect(FactoryBot.build(:order)).to be_valid
       end
     end
+
+    context 'with invalid attributes' do
+      it 'invalid without total_price' do
+        order = FactoryBot.build(:invalid_order)
+        order.valid?
+        expect(order.errors[:total_price]).to include("can't be blank")
+      end
+    end
   end
 end

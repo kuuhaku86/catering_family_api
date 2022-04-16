1a2a4b4c5b3a6fcf6aeb4ac20d4a96e94a99def8
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-16 10:57:54 +0700

    Add validation for Customer model, invalid without name

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Customer, type: :model do
         expect(FactoryBot.build(:customer)).to be_valid
       end
     end
+
+    context 'with invalid attributes' do
+      it 'invalid without name' do
+        customer = FactoryBot.build(:invalid_customer)
+        customer.valid?
+        expect(customer.errors[:name]).to include("can't be blank")
+      end
+    end
   end
 end

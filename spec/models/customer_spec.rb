8f116ed70bab9c5d4e12fc6f9ec6b541b60ce1e4
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-16 10:54:46 +0700

    Add validation for Customer mode, is valid with a name and a email

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.factory' do
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:customer)).to be_valid
+    end
+  end
+
+  describe '.validations' do
+    context 'with valid attributes' do
+      it 'is valid with a name and a email' do
+        expect(FactoryBot.build(:customer)).to be_valid
+      end
+    end
+  end
 end

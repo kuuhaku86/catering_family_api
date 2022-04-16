8053d0e242a832fa9e2d9bcd5e5c259f523f8320
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-16 11:10:46 +0700

    Add factory for Order model

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :order do
     total_price { 1.5 }
+    status { "new" }
+  end
+
+  factory :invalid_order, parent: :order do
+    total_price { nil }
+    status { nil }
   end
 end

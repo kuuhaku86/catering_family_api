233bc1c4c97a5ada9dd1f0056d787dcf4b8ca365
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2022-04-17 19:42:56 +0700

    Add MenusController for UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :menus, only: [:index]
+
   namespace :api do
     resources :categories
     resources :menus
